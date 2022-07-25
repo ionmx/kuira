65e23b10092024812b3125d1516e32a520673711
Jonathan Hernández <jonathan.hernandez@gmail.com>
2022-07-25 14:45:59 -0600

    Validate association

--- spec/models/space_member_spec.rb
+++ spec/models/space_member_spec.rb
@@ -5,11 +5,11 @@ require 'rails_helper'
 RSpec.describe(SpaceMember, type: :model) do
   describe 'validations' do
     describe 'space' do
-      it { is_expected.to validate_presence_of(:space) }
+      it { is_expected.to belong_to(:space) }
     end
 
     describe 'user' do
-      it { is_expected.to validate_presence_of(:user) }
+      it { is_expected.to belong_to(:user) }
     end
   end
 end
--- spec/models/space_spec.rb
+++ spec/models/space_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe(Space, type: :model) do
     end
 
     describe 'user' do
-      it { is_expected.to validate_presence_of(:user) }
+      it { is_expected.to belong_to(:user) }
     end
   end
 end
